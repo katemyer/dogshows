d4344c2a3894596d81c3d40e5de0533e85865241
kate.m <kate.mangubat@highspot.com>
2023-01-17 14:45:19 -0800

    initial setup

---
+++ src/About/About.js
@@ -0,0 +1,13 @@
+import './About.css'
+const About = () => {
+    return (
+      <div className="about-container">
+          <div>
+            <h1>AKC Show Calculator</h1>
+            We did the maths, do you don't have to<br/>
+          </div>
+      </div>
+    );
+  };
+
+export default About;
